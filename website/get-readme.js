b910601f8422e2e89b4f4afadb37f79c0cf884ca
Martin Heidegger <martin.heidegger@gmail.com>
2019-03-07 02:53:43 +0900

    Fixes for #32 (#34)
    * Fixes for #32
    
    * no message

--- website/get-readme.js
+++ website/get-readme.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs'
 import path from 'path'
 
 (async () => {
-  const rawReadme = await fs.readFile(path.join(__dirname, '..', 'Readme.md'))
+  const rawReadme = await fs.readFile(path.join(__dirname, '..', 'README.md'))
   const html = await markdowner(rawReadme)
 
   await fs.writeFile(`readme.json`, JSON.stringify(html, null, 2))
